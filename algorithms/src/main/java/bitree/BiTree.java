45f4e9776d3c2af3519f65592c4b0ae40e82defc
tencent_sainty <saintyyu@tencent.com>
2021-04-29 22:19:33 +0800

    二叉树

---
+++ algorithms/src/main/java/bitree/BiTree.java
@@ -0,0 +1,27 @@
+package bitree;
+
+/**
+ * @description:
+ * @author: saintyyu
+ * @date: 2021/4/18 11:00 下午
+ */
+public class BiTree {
+    private BiTree left;
+    private BiTree right;
+
+    public BiTree getLeft() {
+        return left;
+    }
+
+    public void setLeft(BiTree left) {
+        this.left = left;
+    }
+
+    public BiTree getRight() {
+        return right;
+    }
+
+    public void setRight(BiTree right) {
+        this.right = right;
+    }
+}
---
+++ algorithms/src/main/java/bitree/BiTreeHeight.java
@@ -0,0 +1,25 @@
+package bitree;
+
+/**
+ * @description:
+ * @author: saintyyu
+ * @date: 2021/4/18 11:00 下午
+ */
+public class BiTreeHeight {
+    /**
+     *问题：求一棵二叉树的高度
+     *分析：这个问题采用递归求解比较简单
+     *思路：依次返回其左右子树的高度，然后分别加1，将更大的值返回。
+     *说明：求一棵二叉树的高度虽然简单，但在其他相关的二叉树操作中却非常有用。
+     * 时间复杂度：O(n)  遍历节点
+     */
+    public static int calcBiTreeHeight(BiTree tree) {
+        int height = 0;
+        if (tree != null) {
+            int leftHeight = calcBiTreeHeight(tree.getLeft());
+            int rightHeight = calcBiTreeHeight(tree.getRight());
+            height = Math.max(leftHeight, rightHeight) + 1;
+        }
+        return height;
+    }
+}
