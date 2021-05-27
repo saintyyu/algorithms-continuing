a9bb0757220da46a5eb02e774d9602a955123f90
tencent_sainty <saintyyu@tencent.com>
2021-05-27 23:28:21 +0800

    排序数组快速检索

---
+++ algorithms/src/main/java/array/FindInSortedArray.java
@@ -0,0 +1,39 @@
+package array;
+
+/**
+ * @description:
+ * @author: saintyyu
+ * @date: 2021/5/27 11:02 下午
+ */
+public class FindInSortedArray {
+
+    /**
+     * 问题：从一个整数数组中快速查找是否存在某个元素，数组每行、每列都是有序的（递增）；
+     * 示例：
+     * {1, 4, 7, 10},
+     * {2, 5, 8, 11},
+     * {3, 6, 9, 12}
+     * 5在数组中，13则不在数组中。
+     * <p>
+     * 分析：对于数组中的任意一个元素，其左边和上边两个元素都比它小，而下边和右边都比他大，因而可以从数组的最左下角开始进行查询，
+     * 如果当前元素和目标元素相等则返回true，如果当前元素比目标元素小，则往右移动；否则往上移动，直到返回true或到达边界返回false。
+     * 时间复杂度（最大值）：O(r+c)，其中r为行数，c为列数
+     */
+    public static boolean exist(int[][] array, int target) {
+        if (array == null) {
+            return false;
+        }
+        int row = array.length - 1;
+        int column = 0;
+        while (row >= 0 && column < array[0].length) {
+            if (array[row][column] == target) {
+                return true;
+            } else if (array[row][column] > target) {
+                row--;
+            } else {
+                column++;
+            }
+        }
+        return false;
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,7 @@
+
+进度：
+
+https://github.com/saintyyu/algorithms/tree/master/algorithm-study/src/main/java/array
+
+
+https://github.com/saintyyu/algorithms/blob/master/algorithm-study/src/main/java/array/FindInArray.java
